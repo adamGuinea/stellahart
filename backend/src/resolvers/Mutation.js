71b214d71983998b068a80221bfab84e3047c5fb
adamGuinea <adamguinea22@gmail.com>
2018-12-30 11:59:09 +1300

    began user signup and permissions

--- backend/src/resolvers/Mutation.js
+++ backend/src/resolvers/Mutation.js
@@ -1,3 +1,6 @@
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+
 const Mutations = {
     async createItem (parent, args, ctx, info) {
         // TODO: check if they're logged in
@@ -34,13 +37,35 @@ const Mutations = {
     async deleteItem(parent, args, ctx, info){
         const where = { id: args.id };
         // 1, find the item
-        const item = await  ctx.db.query.item({where}, `{ id 
-            title}`);
+        const item = await  ctx.db.query.item({where}, `{ id title}`);
         // 2. check if they own the item or have permissions
        
         // 3. delete it
         return ctx.db.mutation.deleteItem({ where }, info);
-    }
+    },
+    async signup(parent, args, ctx, info) {
+        args.email = args.email.toLowerCase();
+        // hash password
+        const password = await bcrypt.hash(args.password, 10);
+        const user = await ctx.db.mutation.createUser(
+            {
+                data: {
+                ...args,
+                password,
+                permissions: {set: ['USER']},
+                },
+            }, 
+            info
+        );
+        // create jwt token for them 
+        const token = jwt.sign({userId: user.id}, process.env.APP_SECRET);
+            ctx.response.cookie('token', token, {
+                httpOnly: true,
+                maxAge: 1000 * 60 * 60 * 24 * 365,
+            });
+        // return to browser
+        return user;
+    },
 };
 
 module.exports = Mutations;
