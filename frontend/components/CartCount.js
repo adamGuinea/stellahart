50ef51bc22b87de70ce8c01b7b142c8b518fa1b9
adamGuinea <adamguinea22@gmail.com>
2019-01-08 16:52:39 +1300

    added autocomplete search and dropdown menu

--- frontend/components/CartCount.js
+++ frontend/components/CartCount.js
@@ -32,7 +32,7 @@ const Dot = styled.div`
     background: ${props => props.theme.red};
     color: white;
     border-radius: 50%;
-    padding: 0.25rem;
+    padding: 0.5rem;
     line-height: 2rem;
     min-width: 3rem;
     margin-left: 1rem;
