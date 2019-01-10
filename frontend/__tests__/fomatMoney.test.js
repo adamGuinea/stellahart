4a770c7427f1b04fcd32364895fd719af23e0581
adamGuinea <adamguinea22@gmail.com>
2019-01-11 11:16:50 +1300

    testing format money fn

---
+++ frontend/__tests__/fomatMoney.test.js
@@ -0,0 +1,18 @@
+import formatMoney from '../lib/formatMoney';
+
+describe('formatMoney Function', () => {
+    it('works with fractional dollars', () => {
+        expect(formatMoney(1)).toEqual('$0.01');
+        expect(formatMoney(30)).toEqual('$0.30');
+    });
+    it('leaves cents off for whole dollars', () => {
+        expect(formatMoney(5000)).toEqual('$50');
+        expect(formatMoney(100)).toEqual('$1');
+        expect(formatMoney(50000000)).toEqual('$500,000');
+    });
+    it('works with whole and fractional dollars', () => {
+        expect(formatMoney(5012)).toEqual('$50.12');
+        expect(formatMoney(101)).toEqual('$1.01');
+        expect(formatMoney(2965098)).toEqual('$29,650.98');
+    });
+});
