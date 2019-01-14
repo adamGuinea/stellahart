c19682d07e6ec276b3d6b896018c41c23074fe3b
adamGuinea <adamguinea22@gmail.com>
2019-01-14 14:44:29 +1300

    removed count variable

--- frontend/components/Pagination.js
+++ frontend/components/Pagination.js
@@ -20,7 +20,7 @@ const Pagination = props => (
     <Query query={PAGINATION_QUERY}>
             {({data, loading, error}) => {
                 if(loading) return <p>Loading...</p>;
-                const count = data.itemsConnection.aggregate.count;
+                // const count = data.itemsConnection.aggregate.count;
                 const pages = Math.ceil(count / perPage);
                 const page = props.page;
                 return (
