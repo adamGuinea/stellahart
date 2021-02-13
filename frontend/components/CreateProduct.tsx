54eeb6a4f778a7fe5ecf5f0e223607ff9ba8488b
adamGuinea <adamguinea22@gmail.com>
2021-02-13 13:30:49 +1000

    uploading products

--- frontend/components/CreateProduct.tsx
+++ frontend/components/CreateProduct.tsx
@@ -1,6 +1,38 @@
+import { useMutation } from '@apollo/client';
+import gql from 'graphql-tag';
 import React from 'react';
 import useForm from '../lib/useForm';
+import DisplayError from './ErrorMessage';
 import Form from './styles/Form';
+
+const CREATE_PRODUCT_MUTATION = gql`
+mutation CREATE_PRODUCT_MUTATION(
+  $name: String!
+  $description: String!
+  $price: Int!
+  $image: Upload
+) {
+  createProduct (
+    data: {
+      name: $name,
+      description: $description,
+      price: $price,
+      status: "AVAILABLE"
+      photo: {
+        create: {
+          image: $image,
+          altText: $name
+        }
+      }
+    }
+  ) {
+    id
+    price
+    description
+    name
+  }
+}
+`;
 
 export default function CreateProduct() {
   const { inputs, handleChange, clearForm, resetForm } = useForm({
@@ -8,9 +40,22 @@ export default function CreateProduct() {
     price: 0,
   });
 
+  const [createProduct, { loading, error, data }] = useMutation(
+    CREATE_PRODUCT_MUTATION,
+    {
+      variables: inputs,
+    }
+  );
+
   return (
-    <Form onSubmit={(e) => e.preventDefault()}>
-      <fieldset>
+    <Form
+      onSubmit={async (e) => {
+        e.preventDefault();
+        const res = await createProduct();
+      }}
+    >
+      <DisplayError error={error} />
+      <fieldset disabled={loading} aria-busy={loading}>
         <label htmlFor="image">
           Image
           <input
